014ff892eb16e3a63059548ac169647f0e6dfc2f
Spencer Dixon <spencercdixon@gmail.com>
2015-02-02 17:37:53 -0500

    Properly set up scope for the #getChoice() method in the computer model

--- public/rps.js
+++ public/rps.js
@@ -4,8 +4,9 @@ window.RPS = {
 }
 
 window.RPS.Models.Player = function(name) {
-  this.name = name
-  this.score = 0
+  this.that = this;
+  this.name = name;
+  this.score = 0;
 
   this.getChoice = function() {
     this.choice = prompt("What do you choose? R/P/S?");
@@ -13,15 +14,13 @@ window.RPS.Models.Player = function(name) {
 }
 
 window.RPS.Models.Computer = function () {
-  var randomNumber = Math.floor((Math.random() * 3) + 1);
-  var choices = ["r", "p", "s"];
-
+  this.that = this;
   this.name = "Computer";
-  this.choice = choices[randomNumber];
 
   this.getChoice = function() {
-    debugger;
-    this.choice = choices[randomNumber];
+    var randomNumber = Math.floor((Math.random() * 3) + 1);
+    var choices = ["r", "p", "s"];
+    return this.choice = choices[randomNumber];
   }
 }
 
@@ -48,11 +47,12 @@ window.RPS.Models.ScoreCalculator = function(player_choice, computer_choice) {
 // Sanity check for creating JS objects
 var player = new window.RPS.Models.Player("spencer");
 var computer = new window.RPS.Models.Computer();
-debugger;
 
 player.getChoice();
-
 console.log(player.choice);
+
+computer.getChoice();
+console.log(computer.choice);
 
 var calc = new window.RPS.Models.ScoreCalculator(player.choice, computer.choice);
 
